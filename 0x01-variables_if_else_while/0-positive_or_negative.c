7caaac714a26d3201fa5925d024552dce3005b40
Maxi254 <ocholamaxwell543@gmail.com>
2022-12-09 06:37:22 -0800

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive\n" n);
+		printf("%i is positive\n" n);
 	}
 	else if (n == 0)
 	{
@@ -25,4 +25,4 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 	return (0);
-i}
+}
