5523caba7c986d98a5b59cddfc550bd4ad3d2a6e
Maxi254 <ocholamaxwell543@gmail.com>
2023-06-08 13:45:39 -0700

    done

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,15 +12,11 @@ int is_primeNum(unsigned int n, unsigned int a)
 	if (n % a == 0)
 	{
 		if (n == a)
-		{
 			return (1);
-		}
 		else
-		{
 			return (0);
-		}
-		return (0 + is_primeNum(n, a + 1));
 	}
+	return (0 + is_primeNum(n, a + 1));
 }
 /**
  * is_prime_number - finds if an input number is a prime number.
