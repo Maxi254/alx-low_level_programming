d3c2d6f92aa37b7b9222aa5c6c10e1707e5a683c
Maxi254 <ocholamaxwell543@gmail.com>
2023-06-11 08:22:38 -0700

    six

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - processes the absolute value of a number
+ * @x: has an integer value as an argument
+ * Return: absolute value
+ */
+
+int _abs(int x)
+{
+	int k = -1;
+
+	if (x < 0)
+	{
+		return (x * k);
+	}
+	else
+	{
+		return (x);
+	}
+}
