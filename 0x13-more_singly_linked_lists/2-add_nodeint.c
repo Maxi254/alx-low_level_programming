32b852bc1eb0d3ce127e87766b68ea74e8e91c1c
Maxi254 <ocholamaxwell543@gmail.com>
2023-07-05 18:42:00 -0700

    returns node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds new node to beginn of the func
+ * @head: head
+ * @n: n elem
+ * Return: address of the new element. NUll if it failed.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_nod = malloc(sizeof(listint_t));
+
+	if (new_nod == NULL)
+		return (NULL);
+
+	new_nod->n = n;
+	new_nod->next = *head;
+
+	*head = new_nod;
+
+	return (*head);
+}
