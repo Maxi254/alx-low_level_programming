ef649c03399ce2665e00578f717d0571238da20a
Maxi254 <ocholamaxwell543@gmail.com>
2023-07-05 18:53:31 -0700

    lool

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -23,6 +23,11 @@ typedef struct listint_s
  * @next: ptr to next node
  * Description: singlyy li o ptr
  */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
