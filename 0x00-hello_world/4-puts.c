94393bbad29bcfc7c576503b609b48bd696b82f5
Maxi254 <ocholamaxwell543@gmail.com>
2025-01-09 16:10:00 +0300

    changed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /*
- * main - entry point
+ * main - Entry point
  *
  * Return - zero(0)
- *
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
