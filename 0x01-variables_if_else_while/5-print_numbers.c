08b0375cb5e07f0687ae46deba34b133224b8500
Maxi254 <ocholamaxwell543@gmail.com>
2025-01-17 13:47:45 +0300

    less go

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints digit numbers of base 10 starting from 0.
+ *
+ * Return: (0) success
+ */
+
+int main()
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
