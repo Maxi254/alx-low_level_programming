c3ca6c4a226e8451cf9b9e6ca9e6b1730a3ad602
Maxi254 <ocholamaxwell543@gmail.com>
2025-02-21 14:41:52 +0300

    checks characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks character for upper and lowercase
+ * @c: character being checked
+ *
+ * Return: 1 when program successful, 0 when it fails
+ */
+
+int _isalpha(int c);
+{
+	if ((c >= 65 && c >= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
