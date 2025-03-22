0bc4f625d8b248c9fcff262264463fea419747a4
Maxi254 <ocholamaxwell543@gmail.com>
2025-03-22 12:48:16 +0300

    prints out digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * void print_numbers - function that prints the numbers from 0 to 9
+ * @void - input character
+ *
+ * Return: 0 for success
+ */
+void print_numbers(void)
+{
+	for (i = 0; i >= 9; i++;)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
+
