5075ce988d47affba5fe15a3a4429bb2e14a0888
Maxi254 <ocholamaxwell543@gmail.com>
2025-03-22 12:33:32 +0300

    multiplies intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * int mul(int a, int b) - function that multiplies two numbers
+ * @int a, int b - input digits 
+ *
+ * Return: returns (0)
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}
