d110e4d2d62963e2b1775b37e2f15f3269235c69
Maxi254 <ocholamaxwell543@gmail.com>
2025-05-28 16:09:15 +0300

    okay

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * strlen - returns the string length 
+ * @*c - points to the first character being typed
+ *
+ * Return: (0) for success
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+	while (str[len] =! '\0')
+	{
+		len++;
+	}
+	return len;
+}
+		
+
